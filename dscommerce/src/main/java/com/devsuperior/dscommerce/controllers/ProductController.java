f2e298c915613433dbbf80f60081a67c1127592f
AlexRitter1 <ritter.alex@hotmail.com>
2025-06-28 22:19:23 -0300

    add: @PreAuthorized

--- dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,73 +1,60 @@
 package com.devsuperior.dscommerce.controllers;
 
-import com.devsuperior.dscommerce.DTO.ProductDTO;
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.dto.ProductMinDTO;
 import com.devsuperior.dscommerce.services.ProductService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
 
     @Autowired
-    private ProductService productService;
+    private ProductService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<ProductDTO> findByID(@RequestBody @PathVariable Long id){
-        ProductDTO dto = productService.findById(id);
+    public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
+        ProductDTO dto = service.findById(id);
         return ResponseEntity.ok(dto);
     }
 
     @GetMapping
-    public ResponseEntity<Page<ProductDTO>> searchByName(
-            @RequestParam(name = "name", defaultValue = "") String name, Pageable pageable){
-        Page<ProductDTO> dto = productService.findByName(name, pageable);
+    public ResponseEntity<Page<ProductMinDTO>> findAll(
+            @RequestParam(name = "name", defaultValue = "") String name,
+            Pageable pageable) {
+        Page<ProductMinDTO> dto = service.findAll(name, pageable);
         return ResponseEntity.ok(dto);
     }
 
-    /*@GetMapping
-    public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable){
-        var dto = productService.findAll(pageable); //retorna uma lista de objetos do tipo ProductDTO
-        return ResponseEntity.ok(dto);
-    }*/
-
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping
-    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO productDTO){
-        var dto = productService.insert(productDTO);
-        var uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+    public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                 .buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).body(dto);
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PutMapping(value = "/{id}")
-    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO productDTO){
-        productDTO = productService.update(id, productDTO);
-        return ResponseEntity.ok(productDTO);
+    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @Valid @RequestBody ProductDTO dto) {
+        dto = service.update(id, dto);
+        return ResponseEntity.ok(dto);
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping(value = "/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id){
-        productService.delete(id);
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
         return ResponseEntity.noContent().build();
     }
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
