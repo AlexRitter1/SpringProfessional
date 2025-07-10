a8e64a177315454f0911ef570185924e29675044
AlexRitter1 <ritter.alex@hotmail.com>
2025-07-09 21:48:22 -0300

    fix: service bugs

--- dscommerce/src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -1,12 +1,5 @@
 package com.devsuperior.dscommerce.services;
 
-import com.devsuperior.dscommerce.dto.ProductDTO;
-import com.devsuperior.dscommerce.dto.ProductMinDTO;
-import com.devsuperior.dscommerce.entities.Product;
-import com.devsuperior.dscommerce.repositories.ProductRepository;
-import com.devsuperior.dscommerce.services.exceptions.DatabaseException;
-import com.devsuperior.dscommerce.services.exceptions.ResourceNotFoundException;
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -15,76 +8,81 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscommerce.dto.CategoryDTO;
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.dto.ProductMinDTO;
+import com.devsuperior.dscommerce.entities.Category;
+import com.devsuperior.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.repositories.ProductRepository;
+import com.devsuperior.dscommerce.services.exceptions.DatabaseException;
+import com.devsuperior.dscommerce.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class ProductService {
 
     @Autowired
-    private ProductRepository productRepository;
+    private ProductRepository repository;
 
     @Transactional(readOnly = true)
-    public Page<ProductDTO> findByName(String name, Pageable pageable) {
-        var result = productRepository.searchByName(name, pageable);
-        return result.map(r -> new ProductDTO(r));
-    }
-
-    @Transactional(readOnly = true)
-    public ProductDTO findById(Long id){
-        var product = productRepository.findById(id).orElseThrow(
-                () -> new ResourceNotFoundException("Produto não encontrado"));
+    public ProductDTO findById(Long id) {
+        Product product = repository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Recurso não encontrado"));
         return new ProductDTO(product);
     }
 
     @Transactional(readOnly = true)
-    public Page<ProductMinDTO> findAll(String name, Pageable pageable){
-        Page<Product> results = productRepository.searchByName(name, pageable);
-        return results.map(p -> new ProductMinDTO(p));
+    public Page<ProductMinDTO> findAll(String name, Pageable pageable) {
+        Page<Product> result = repository.searchByName(name, pageable);
+        return result.map(x -> new ProductMinDTO(x));
     }
 
     @Transactional
-    public ProductDTO insert(ProductDTO productDTO){
-        productRepository.save(new Product(productDTO));
-        return productDTO;
+    public ProductDTO insert(ProductDTO dto) {
+        Product entity = new Product();
+        copyDtoToEntity(dto, entity);
+        entity = repository.save(entity);
+        return new ProductDTO(entity);
     }
 
     @Transactional
-    public ProductDTO update(Long id, ProductDTO productDTO){
+    public ProductDTO update(Long id, ProductDTO dto) {
         try {
-            Product product = productRepository.getReferenceById(id);
-            dtoToProduct(product, productDTO);
-            product = productRepository.save(product);
-            return new ProductDTO(product);
+            Product entity = repository.getReferenceById(id);
+            copyDtoToEntity(dto, entity);
+            entity = repository.save(entity);
+            return new ProductDTO(entity);
         }
-        catch (EntityNotFoundException e){
-            throw new ResourceNotFoundException("Produto não encontrado");
+        catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Recurso não encontrado");
         }
     }
 
     @Transactional(propagation = Propagation.SUPPORTS)
     public void delete(Long id) {
-        if (!productRepository.existsById(id)) {
+        if (!repository.existsById(id)) {
             throw new ResourceNotFoundException("Recurso não encontrado");
         }
         try {
-            productRepository.deleteById(id);
+            repository.deleteById(id);
         }
         catch (DataIntegrityViolationException e) {
             throw new DatabaseException("Falha de integridade referencial");
         }
     }
 
+    private void copyDtoToEntity(ProductDTO dto, Product entity) {
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgUrl(dto.getImgUrl());
 
-
-
-
-    private void dtoToProduct(Product product, ProductDTO productDTO) {
-            if (productDTO == null || product == null) {
-                throw new IllegalArgumentException("Product or ProductDTO cannot be null");
-            }
-            // Copia os campos do DTO para a entidade Product
-            product.setName(productDTO.getName());
-            product.setDescription(productDTO.getDescription());
-            product.setPrice(productDTO.getPrice());
-            product.setImgUrl(productDTO.getImgUrl());
+        entity.getCategories().clear();
+        for (CategoryDTO catDto : dto.getCategories()) {
+            Category cat = new Category();
+            cat.setId(catDto.getId());
+            entity.getCategories().add(cat);
         }
-
     }
+}
--- dscommerce/src/main/java/com/devsuperior/dscommerce/services/UserService.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/services/UserService.java
@@ -1,9 +1,7 @@
 package com.devsuperior.dscommerce.services;
-import com.devsuperior.dscommerce.dto.UserDTO;
-import com.devsuperior.dscommerce.entities.Role;
-import com.devsuperior.dscommerce.entities.User;
-import com.devsuperior.dscommerce.projections.UserDetailsProjection;
-import com.devsuperior.dscommerce.repositories.UserRepository;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -13,32 +11,37 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import java.util.List;
+
+import com.devsuperior.dscommerce.dto.UserDTO;
+import com.devsuperior.dscommerce.entities.Role;
+import com.devsuperior.dscommerce.entities.User;
+import com.devsuperior.dscommerce.projections.UserDetailsProjection;
+import com.devsuperior.dscommerce.repositories.UserRepository;
 
 @Service
 public class UserService implements UserDetailsService {
 
 	@Autowired
 	private UserRepository repository;
-	
+
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		
+
 		List<UserDetailsProjection> result = repository.searchUserAndRolesByEmail(username);
 		if (result.size() == 0) {
 			throw new UsernameNotFoundException("Email not found");
 		}
-		
+
 		User user = new User();
 		user.setEmail(result.get(0).getUsername());
 		user.setPassword(result.get(0).getPassword());
 		for (UserDetailsProjection projection : result) {
 			user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
 		}
-		
+
 		return user;
 	}
-	
+
 	protected User authenticated() {
 		try {
 			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -50,7 +53,7 @@ public class UserService implements UserDetailsService {
 			throw new UsernameNotFoundException("Invalid user");
 		}
 	}
-	
+
 	@Transactional(readOnly = true)
 	public UserDTO getMe() {
 		User entity = authenticated();
