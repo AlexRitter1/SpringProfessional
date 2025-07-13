d17a5b37222961e8f9c972f98168f0ec9d7959b7
AlexRitter1 <ritter.alex@hotmail.com>
2025-07-13 12:03:59 -0300

    fixing bugs

--- dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/OrderController.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/OrderController.java
@@ -1,14 +1,18 @@
 package com.devsuperior.dscommerce.controllers;
-
-import com.devsuperior.dscommerce.dto.OrderDTO;
-import com.devsuperior.dscommerce.services.OrderService;
-import jakarta.validation.Valid;
+import java.net.URI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import java.net.URI;
+import com.devsuperior.dscommerce.dto.OrderDTO;
+import com.devsuperior.dscommerce.services.OrderService;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/orders")
@@ -23,7 +27,7 @@ public class OrderController {
         OrderDTO dto = service.findById(id);
         return ResponseEntity.ok(dto);
     }
-    
+
     @PreAuthorize("hasRole('ROLE_CLIENT')")
     @PostMapping
     public ResponseEntity<OrderDTO> insert(@Valid @RequestBody OrderDTO dto) {
@@ -31,5 +35,5 @@ public class OrderController {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                 .buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).body(dto);
-    } 
+    }
 }
--- dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/UserController.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/UserController.java
@@ -1,13 +1,13 @@
 package com.devsuperior.dscommerce.controllers;
 
-import com.devsuperior.dscommerce.dto.UserDTO;
-import com.devsuperior.dscommerce.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.devsuperior.dscommerce.dto.UserDTO;
+import com.devsuperior.dscommerce.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -19,7 +19,7 @@ public class UserController {
     @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
     @GetMapping(value = "/me")
     public ResponseEntity<UserDTO> getMe() {
-    	UserDTO dto = service.getMe();
+        UserDTO dto = service.getMe();
         return ResponseEntity.ok(dto);
     }
 }
